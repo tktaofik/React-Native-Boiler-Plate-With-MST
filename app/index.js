b220cd8b3e966d0fc5a2409de7f1ae172146c31b
tktaofik <tktaofik@users.noreply.github.com>
2018-03-20 21:35:57 -0400

    added redux dev tools

--- app/index.js
+++ app/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { asReduxStore, connectReduxDevtools } from 'mst-middlewares';
+import { ItemList } from './stores/sampleStore'
 import Navigation from './navigation';
 
-export default () => <Navigation/>;
+const storeState = ItemList.create({
+  items: [
+    {
+      name: 'Chronicles of Narnia Box Set - C.S. Lewis',
+      price: 28.73,
+    },
+  ],
+});
+
+window.sampleStore = storeState;
+
+const store = asReduxStore(storeState);
+connectReduxDevtools(require('remotedev'), storeState);
+
+export default () => (<Provider store={store}><Navigation/></Provider>);
